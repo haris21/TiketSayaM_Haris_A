4537113ad973333403904adf69f10c5a8cb399ce
haris alifi <harismlg07@gmail.com>
2019-10-23 07:44:49 +0700

    Update with firebase on reg page one

--- app/src/main/java/com/example/tiketsayam_haris_a/RegisterOneAct.java
+++ app/src/main/java/com/example/tiketsayam_haris_a/RegisterOneAct.java
@@ -1,38 +1,89 @@
 package com.example.tiketsayam_haris_a;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterOneAct extends AppCompatActivity {
     public LinearLayout btnback;
     public Button btncontinue;
+    public EditText username, password, email;
+    DatabaseReference reference;
+
+    String USERNAME_KEY = "usernamekey";
+    String username_key = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_one);
-
+        //load variabel
         btnback = findViewById(R.id.btnBack);
         btncontinue = findViewById(R.id.btnContinue);
+        username = findViewById(R.id.edtUserName);
+        password = findViewById(R.id.edtPassword);
+        email = findViewById(R.id.edtEmail);
+
+        //load function
+        //goto prev page
         btnback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Call Intent
                 Intent gotosignin = new Intent(RegisterOneAct.this, SignInAct.class);
                 startActivity(gotosignin);
             }
         });
 
+        //goto next page
         btncontinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gototoregistertwo=new Intent(RegisterOneAct.this,RegisterTwo.class);
+                //save data to local storage
+                SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(username_key, username.getText().toString());
+                editor.apply();
+
+
+                //Toast.makeText(getApplicationContext(), "User nme" + username.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                //save databases
+                reference = FirebaseDatabase.getInstance().getReference().child("Users").child(username.getText().toString());
+                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        
+                        dataSnapshot.getRef().child("username").setValue(username.getText().toString());
+                        dataSnapshot.getRef().child("password").setValue(password.getText().toString());
+                        dataSnapshot.getRef().child("email_address").setValue(email.getText().toString());
+                        dataSnapshot.getRef().child("user_balance").setValue(email.getText().toString());
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+                Intent gototoregistertwo = new Intent(RegisterOneAct.this, RegisterTwo.class);
                 startActivity(gototoregistertwo);
             }
         });
     }
 }
+
